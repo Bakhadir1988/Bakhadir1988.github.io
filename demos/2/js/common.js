09bec774ee485cc0039d1b6f9953ae4727fb125d
Bakhadir1988 <myblogfree1988@gmail.com>
2016-12-16 13:44:47 +0300

    3

--- demos/2/js/common.js
+++ demos/2/js/common.js
@@ -1,5 +1,38 @@
 $(function() {
 
-	// Custom JS
+    $("a[href='#callback']").magnificPopup();
 
-});
+    $(".toggle-mnu").click(function() {
+        $(this).toggleClass("on");
+        $(".header__menu").slideToggle();
+        return false;
+    }); 
+
+
+    var owl = $(".owl-carousel");
+    owl.owlCarousel({
+    	items : 1,
+    	nav : true,
+    	navText : "",
+    	loop : true,
+    	dots: false,
+    	autoplay : true,
+    	autoplayHoverPause : true,
+    	fluidSpeed : 600,
+    	itemClass: "slide",
+    	autoplaySpeed : 600,
+    	navSpeed : 000,
+    	dotsSpeed : 600,
+    	dragEndSpeed : 600
+    });
+    $(".next").click(function(){
+    	owl.trigger("next.owl.carousel");
+    })
+    $(".prev").click(function(){
+    	owl.trigger("prev.owl.carousel");
+    })
+
+    
+
+    
+  });
